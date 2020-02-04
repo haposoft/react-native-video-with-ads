ce57d702963f1ce3960cd2d894d8ab52947a7d42
kienphan <senior1206@gmail.com>
2020-02-04 16:39:58 +0700

    add play function

--- Video.js
+++ Video.js
@@ -67,6 +67,10 @@ export default class Video extends Component {
     }
   };
 
+  play = () => {
+    this.setNativeProps({ paused: false });
+  };
+
   presentFullscreenPlayer = () => {
     this.setNativeProps({ fullscreen: true });
   };
